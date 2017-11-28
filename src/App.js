93c52941d5d265a8d03903d90af9955a55a1874d
hhb0508 <272396657@qq.com>
2017-11-28 10:18:57 +0800

    '2'

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React,{Component}  from 'react';
 import './App.css';
 import Header from './components/Header/Header.js';
 import './components/Header/Header.scss';
-import Searchipt from './components/Search/Searchipt.js';
 class App extends Component{
   render(){
     return (
         <div className="box">
             <Header></Header>
-            <Searchipt></Searchipt>
-            <Footer></Footer>
+            
         </div>
       );
   }
