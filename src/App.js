7f56c96ed5ea42cd0f434f29d0918341b32ab82d
hhb0508 <272396657@qq.com>
2017-11-28 10:30:04 +0800

    'a'

--- src/App.js
+++ src/App.js
@@ -2,17 +2,14 @@ import React,{Component}  from 'react';
 import './App.css';
 import Header from './components/Header/Header.js';
 import './components/Header/Header.scss';
-<<<<<<< HEAD
-=======
 import Searchipt from './components/Search/Searchipt.js';
 import Footer from './components/Footer/Footer.js';
->>>>>>> 98bb2ed958299de82f898dd4202ac028bf66109b
 class App extends Component{
   render(){
     return (
         <div className="box">
             <Header></Header>
-            
+            <Footer></Footer>
         </div>
       );
   }
